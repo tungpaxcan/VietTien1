60a9b005c930eb330b4642fd19197439cffb6be2
Hoang.NNM <85442015+hoanghg45@users.noreply.github.com>
2022-09-14 15:44:01 +0700

    hee

---
+++ iGMS/Controllers/AppWareHouseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iGMS.Controllers
+{
+    public class AppWareHouseController : Controller
+    {
+        // GET: AppWareHouse
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- iGMS/Controllers/RFIDController.cs
+++ iGMS/Controllers/RFIDController.cs
@@ -85,5 +85,6 @@ namespace iGMS.Controllers
 
             return ("");
         }
+
     }
 }
